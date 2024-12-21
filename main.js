390ac3c6a6fb2cdcf8bfe0ae290271f95f41fe5d
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2024-12-21 18:46:03 +0500

    2-dars

---
+++ main.js
@@ -0,0 +1,39 @@
+// let mars = "Hello";
+// let bu = 123;
+// let kelajak = true;
+// let mars2 = 123n;
+// let marsItSchool;
+// let frontend = null;
+// let salomDunyo = Symbol("id");
+// let obj = { name: "Ali" };
+
+
+// console.log(typeof mars)
+// console.log(typeof bu)
+// console.log(typeof kelajak)
+// console.log(typeof mars2)
+// console.log(typeof marsItSchool)
+// console.log(typeof frontend)
+// console.log(typeof salomDunyo)
+// console.log(typeof obj)
+
+
+// let son1 = +prompt("son kiriting")
+// let son2 = +prompt("son kiritin")
+
+
+// let natija = son1 + son2
+
+// console.log(natija)
+
+
+// let ism =prompt('ismingiz')
+// let yosh =prompt('yoshingiz')
+
+// alert(`mening ismim ${ism} \n mening yoshim ${yosh} da`)
+
+let string = prompt('string typedagi malumot kiriting')
+let number = +prompt('number typedagi malumot kiriting')
+let booolean = prompt('boolean typedagi malumot kiriting')
+confirm = ('hamma malumotlaringiz togri ekan ligiga ishonch hosil qildingizmi?')
+alert(`siz kiritgan ${string} string typega tegishli \n siz kiritgan ${number} number type ga tegishli \n siz kiritgan ${booolean} boolean typega tegishli`);
